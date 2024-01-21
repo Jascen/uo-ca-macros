cd45fdd30bc14d57ec6f699cd3099219abc2bba7
Devin Ulrey <Devin.Ulrey@gmail.com>
2024-01-21 09:32:36 -0500

    - Add SkillItem

--- lib/craft/item.py
+++ lib/craft/item.py
@@ -8,3 +8,16 @@ class CraftableItem:
         
     def __str__(self):
         return self.name
+
+
+class SkillItem:
+    def __init__(self, item, material, min_level, max_level):
+        self.item = item
+        self.min_level = min_level
+        self.max_level = max_level
+
+        if not isinstance(material, list): material = [material] # Convert a single entity into an array
+        self.material = material
+        
+    def __str__(self):
+        return "{} ({} to {})".format(self.item.name, self.min_level, self.max_level)
