b4db00b94e9f250c3e7c3082fe3be19e42cf704d
Devin Ulrey <Devin.Ulrey@gmail.com>
2024-01-22 07:46:04 -0500

    - Allow one or many craft resources

--- lib/craft/item.py
+++ lib/craft/item.py
@@ -11,13 +11,13 @@ class CraftableItem:
 
 
 class SkillItem:
-    def __init__(self, item, material, min_level, max_level):
+    def __init__(self, item, craft_resource_or_array, min_level, max_level):
         self.item = item
         self.min_level = min_level
         self.max_level = max_level
 
-        if not isinstance(material, list): material = [material] # Convert a single entity into an array
-        self.material = material
+        if not isinstance(craft_resource_or_array, list): craft_resource_or_array = [craft_resource_or_array] # Convert a single entity into an array
+        self.resources = craft_resource_or_array
         
     def __str__(self):
         return "{} ({} to {})".format(self.item.name, self.min_level, self.max_level)
