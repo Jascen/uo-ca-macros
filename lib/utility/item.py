4b2fa19b5e2c364c1fbdd5e43debd1fc643ee931
Devin Ulrey <Devin.Ulrey@gmail.com>
2024-01-26 09:07:25 -0500

    - Add method summaries - Clarify variable data - Switch "MoveToOffset" to use X, Y, Z offsets instead of "Location" model

--- lib/utility/item.py
+++ lib/utility/item.py
@@ -5,22 +5,24 @@ from ClassicAssist.Data.Macros.Commands.ObjectCommands import FindType, IgnoreOb
 
 class ItemUtils:
     @classmethod
-    def Move(cls, graphic, hue, distance, source, destination, ignore_destination_first = False):
+    def Move(cls, graphic, hue, distance, source_serial_or_alias, destination_serial_or_alias, ignore_destination_first = False):
+        """Moves the items to the destination container"""
         if ignore_destination_first:
-            while FindType(graphic, distance, destination, hue): IgnoreObject("found")
+            while FindType(graphic, distance, destination_serial_or_alias, hue): IgnoreObject("found")
 
-        cls.__Move(graphic, hue, distance, source, lambda item: MoveItem(item, destination))
-
-
-    @classmethod
-    def MoveToOffset(cls, graphic, hue, distance, source, destination):
-        cls.__Move(graphic, hue, distance, source, lambda item: MoveItemOffset(item, destination.x, destination.y, destination.z))
+        cls.__Move(graphic, hue, distance, source_serial_or_alias, lambda item: MoveItem(item, destination_serial_or_alias))
 
 
     @classmethod
-    def __Move(cls, graphic, hue, distance, source, move_fn):
+    def MoveToOffset(cls, graphic, hue, distance, source_serial_or_alias, x, y, z):
+        """Moves the items to the ground offset position"""
+        cls.__Move(graphic, hue, distance, source_serial_or_alias, lambda item: MoveItemOffset(item, x, y, z))
+
+
+    @classmethod
+    def __Move(cls, graphic, hue, distance, source_serial_or_alias, move_fn):
         items = []
-        while FindType(graphic, distance, source, hue):
+        while FindType(graphic, distance, source_serial_or_alias, hue):
             items.append(GetAlias("found"))
             IgnoreObject("found")
                 
